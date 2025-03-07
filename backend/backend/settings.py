b7603be3d5abda85554d579bc6c37c15f770b283
Declan Munene <munene.declan1@gmail.com>
2025-03-07 19:23:24 +0300

    Done

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["beisawa_completed.onrender.com", "127.0.0.1"]
+ALLOWED_HOSTS = ["beisawa-completed.onrender.com", "127.0.0.1"]
 
 
 # Application definition
