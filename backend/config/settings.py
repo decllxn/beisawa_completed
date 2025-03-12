bec80cb477158f4a67a3fbcf4bde482b75f8f5e6
Declan Munene <munene.declan1@gmail.com>
2025-03-12 15:17:47 +0300

    Link urls

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -76,15 +76,13 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    #"https://supermarketbeisawa.netlify.app",
-    "http://localhost:5173",
+    "https://supermarketbeisawa.netlify.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = [
-    #"https://supermarketbeisawa.netlify.app",
-    "http://localhost:5173",
+    "https://supermarketbeisawa.netlify.app",
 ]
 
 ROOT_URLCONF = 'config.urls'
--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,8 +4,7 @@ import { getCSRFToken } from "../utils/csrf";
 
 
 // Set environment-based API base URL
-//const BASE_URL = "https://beisawa-completed.onrender.com";
-const BASE_URL = "http://127.0.0.1:8000"
+const BASE_URL = "https://beisawa-completed.onrender.com";
 const API_BASE_URL = `${BASE_URL}/offers/api/`;
 const API_BASE_URL2 = `${BASE_URL}/blog/api/`;
 
